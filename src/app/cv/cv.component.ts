1d93a14786f468caf18c7589c55a2e41d3b4728a
Ibrahim Addou <ibrahimaddou2@gmail.com>
2024-05-15 18:04:07 +0200

    filtrer les projets 2

--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -7,6 +7,15 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./cv.component.css']
 })
 export class CVComponent {
+  project:Project = {
+    id:0,
+    name:'Site Portfolio Angular',
+    description:'Projet fait avec angular',
+    projectLink:'github.com/ibrahimaddou',
+    tags:['Angular'],
+    pictures:[]
+
+  }
   constructor(private titleService: Title ){
     this.titleService.setTitle('Ibrahim Addou - CV');
   }
