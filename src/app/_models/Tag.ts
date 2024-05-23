936cff33310f4c5a5decfa2264f4253a8a53bc5b
Ibrahim Addou <ibrahimaddou2@gmail.com>
2024-05-23 21:28:39 +0200

    Pop up - pour le bouton plus 5 les tags

--- src/app/_models/Tag.ts
+++ src/app/_models/Tag.ts
@@ -1,6 +1,7 @@
 export class Tag {
     static readonly ANGULAR = new Tag('Angular','red');
     static readonly C = new Tag('C','blue');
+    static readonly Bootstrap = new Tag('Bootstrap','teal');
 
     private constructor(private readonly key:string, public readonly color:string){
 
